ffe91b44342b93f72c5cbc1aa6673fe9404dbb06
Ivanka58 <kalamaevi0@gmail.com>
2026-02-01 00:21:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,22 @@
 import os
-from dotenv import load_dotenv
-from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+import telebot
 from pyqrcode import create as qr_create
 from io import BytesIO
 from PIL import Image
 
-# Загрузка переменных окружения из .env файла
-load_dotenv()
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+# Получаем токен бота из переменных окружения Render
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
 
-application = ApplicationBuilder().token(TOKEN).build()
-
-async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Команда '/start': приветственное сообщение"""
-    message = (
-        "Привет! Этот бот конвертирует ссылки в QR-коды.\\n"
-        "Просто пришли ему ссылку и он сгенерирует тебе QR-код."
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(
+        message,
+        "Привет! Этот бот конвертирует ссылки в QR-коды.\\nПросто пришли ему ссылку и он сгенерирует тебе QR-код."
     )
-    await update.message.reply_text(message)
 
 def generate_qr(url: str) -> bytes:
-    """Генерация QR-кода по ссылке"""
+    """Генерация QR-кода по ссылке."""
     qr_code = qr_create(url)
     buffer = BytesIO()
     qr_code.png(buffer, scale=6)
@@ -32,40 +27,33 @@ def generate_qr(url: str) -> bytes:
     new_buffer.seek(0)
     return new_buffer.read()
 
-async def link_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Обработка входящих сообщений"""
-    text = update.message.text.strip()
+@bot.message_handler(func=lambda m: True)
+def handle_link(message):
+    text = message.text.strip()
     
     if not text.startswith("http"):
         # Сообщение не является ссылкой
-        reply_message = "Пришли, пожалуйста, именно ссылку!"
-        await update.message.reply_text(reply_message)
+        bot.reply_to(message, "Пришли, пожалуйста, именно ссылку!")
         return
     
     # Отправляем промежуточное сообщение о создании QR-кода
-    temp_msg = await update.message.reply_text("Идёт создание QR-кода...")
+    temp_msg = bot.send_message(message.chat.id, "Идёт создание QR-кода...")
     
     try:
         # Генерируем QR-код
         qr_image_bytes = generate_qr(text)
         
         # Удаляем промежуточное сообщение
-        await context.bot.deleteMessage(chat_id=update.effective_chat.id, message_id=temp_msg.message_id)
+        bot.delete_message(temp_msg.chat.id, temp_msg.message_id)
         
         # Отправляем полученный QR-код
-        await update.message.reply_photo(photo=qr_image_bytes, caption=f"Ваш QR-код готов!")
+        bot.send_photo(message.chat.id, photo=qr_image_bytes, caption="Ваш QR-код готов!")
         
         # Завершаем обработку сообщением благодарности
-        final_message = "Спасибо за использование нашего бота!"
-        await update.message.reply_text(final_message)
+        bot.send_message(message.chat.id, "Спасибо за использование нашего бота!")
     except Exception as e:
         print(e)
-        error_message = "Что-то пошло не так при обработке вашей ссылки :("
-        await update.message.reply_text(error_message)
-
-# Добавляем обработчики
-application.add_handler(CommandHandler('start', start_handler))
-application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), link_handler))
+        bot.reply_to(message, "Что-то пошло не так при обработке вашей ссылки :(")
 
 # Запускаем бота
-application.run_polling()
+bot.infinity_polling()
